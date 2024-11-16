8013f9f675df232d30a6aeb1d156e9ec49715888
Ahmad Najmi <122844294+ahmadnajmi90@users.noreply.github.com>
2024-11-16 16:06:11 +0800

    update dashboard content

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,40 +1,12 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, usePage } from '@inertiajs/react';
 import React from 'react';
+import MainLayout from '../Layouts/MainLayout';
 
-export default function Dashboard() {
-    const { isAdmin, isAcademician } = usePage().props; // Get the user role from the page props
+const Dashboard = () => {
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
-                </h2>
-            }
-        >
-            <Head title="Dashboard" />
-            {isAdmin && (// Show the admin space only if the user is an admin
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                           Admin Space
-                        </div>
-                    </div>
-                </div>
-            </div>
-            )}
-
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                           All user can see this
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </AuthenticatedLayout>
+        <MainLayout title="Dashboard">
+            <p>This is your dashboard content.</p>
+        </MainLayout>
     );
-}
+};
+
+export default Dashboard;
