16bd363900be025e294c53489f21485a3354d714
Ahmad Najmi <122844294+ahmadnajmi90@users.noreply.github.com>
2024-11-16 16:01:04 +0800

    update new routes for grant

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\ProfileCompletionController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Http\Controllers\PostGrantForStudentController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -21,16 +22,27 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/complete-profile', [ProfileCompletionController::class, 'show'])->name('profile.complete');
     Route::post('/complete-profile', [ProfileCompletionController::class, 'update']);
+    Route::get('/dashboard/post-grants', [PostGrantForStudentController::class, 'index'])->name('post-grants.index');
+    Route::get('/dashboard/post-grants/create', [PostGrantForStudentController::class, 'create'])->name('post-grants.create');
+    Route::post('/dashboard/post-grants', [PostGrantForStudentController::class, 'store'])->name('post-grants.store');
+    Route::get('/dashboard/post-grants/{id}/edit', [PostGrantForStudentController::class, 'edit'])->name('post-grants.edit');
+    Route::put('/dashboard/post-grants/{id}', [PostGrantForStudentController::class, 'update'])->name('post-grants.update');
+    Route::delete('/dashboard/post-grants/{id}', [PostGrantForStudentController::class, 'destroy'])->name('post-grants.destroy');
 
     Route::get('/dashboard', function (Request $request) {
         if (!Auth::user()->is_profile_complete) {
             return redirect()->route('profile.complete');
         }
 
+        $postGrants = auth()->user()->postGrants;
+
         return Inertia::render('Dashboard', [
+            'postGrants' => $postGrants,
         ]);
+
     })->name('dashboard');
 });
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
