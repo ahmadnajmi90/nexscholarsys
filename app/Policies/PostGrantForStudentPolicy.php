9b5bd1266b53dff7f0300b5fbde4933f96f079b0
Ahmad Najmi <122844294+ahmadnajmi90@users.noreply.github.com>
2024-11-16 16:00:29 +0800

    Create PostGrantForStudentPolicy.php

---
+++ app/Policies/PostGrantForStudentPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\PostGrantForStudent;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostGrantForStudentPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, PostGrantForStudent $postGrantForStudent): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    use HandlesAuthorization;
+
+    public function update(User $user, PostGrantForStudent $postGrantForStudent)
+    {
+        return $user->unique_id === $postGrantForStudent->academician_id;
+    }
+
+    public function delete(User $user, PostGrantForStudent $postGrantForStudent)
+    {
+        return $user->unique_id === $postGrantForStudent->academician_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, PostGrantForStudent $postGrantForStudent): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, PostGrantForStudent $postGrantForStudent): bool
+    {
+        //
+    }
+}
