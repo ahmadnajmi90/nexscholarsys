2498d9f3eefbdebd1fb4e0926f2c809025383ca4
Ahmad Najmi <122844294+ahmadnajmi90@users.noreply.github.com>
2024-11-23 06:32:53 +0800

    Update PostGrantForStudent.php

--- app/Models/PostGrantForStudent.php
+++ app/Models/PostGrantForStudent.php
@@ -10,16 +10,34 @@ class PostGrantForStudent extends Model
     use HasFactory;
 
     protected $fillable = [
-        'academician_id',
+        'author_id',
         'title',
         'description',
+        'image',
+        'post_status',
+        'grant_status',
+        'category',
+        'tags',
+        'sponsored_by',
+        'location',
+        'email',
+        'contact_number',
+        'purpose',
+        'start_date',
+        'end_date',
+        'budget',
+        'eligibility_criteria',
+        'is_featured',
+        'application_url',
+        'attachment',
     ];
+
 
     /**
      * Define the relationship to the User model (Academician).
      */
     public function academician()
     {
-        return $this->belongsTo(User::class, 'academician_id', 'unique_id'); // Adjust to match your unique identifier field
+        return $this->belongsTo(User::class, 'author_id', 'unique_id'); // Adjust to match your unique identifier field
     }
 }
