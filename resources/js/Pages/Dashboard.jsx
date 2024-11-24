3745843e912988fa2325a2a1bec98238268723b9
Ahmad Najmi <122844294+ahmadnajmi90@users.noreply.github.com>
2024-11-24 21:06:27 +0800

    Update Dashboard.jsx

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -3,13 +3,17 @@ import MainLayout from '../Layouts/MainLayout';
 import RadarChart from '../Components/RadarChart';
 import ProfileCard from '../Components/ProfileCard';
 import DashboardInsights from '../Components/DashboardInsights';
+import SuccessAlert4 from '@/Components/WelcomeMessage';
+
 
 
 const Dashboard = ( {isPostgraduate} ) => {
     console.log(isPostgraduate);
     return (
         <MainLayout title="Dashboard" isPostgraduate={isPostgraduate}>
+            <SuccessAlert4/>
             <DashboardInsights />
+
             {/* <RadarChart />
             <ProfileCard /> */}
         </MainLayout>
