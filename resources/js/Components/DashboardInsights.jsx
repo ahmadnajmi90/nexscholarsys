69e2e1b229a14ecccdac8d81cd230f1d19a7968b
Ahmad Najmi <122844294+ahmadnajmi90@users.noreply.github.com>
2024-11-26 14:01:55 +0800

    Update DashboardInsights.jsx

--- resources/js/Components/DashboardInsights.jsx
+++ resources/js/Components/DashboardInsights.jsx
@@ -6,10 +6,14 @@ import UpcomingEvents from "./UpcomingEvents";
 import RegisteredUser from "./RegsiteredUser";
 import OnlineUser from "./OnlineUser";
 import ClicksByType from "./ClicksByType";
+import SuccessAlert4 from "./WelcomeMessage";
 
 const DashboardInsight  = ( { totalUsers, onlineUsers, clicksByType, isAdmin, events } ) => {
   return (
     <div className="p-6 min-h-screen">
+
+<SuccessAlert4 />
+
       {isAdmin && (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
           <RegisteredUser totalUsers={totalUsers} />
