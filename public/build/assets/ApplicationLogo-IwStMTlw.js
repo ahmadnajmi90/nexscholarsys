93d411adda8512661aee4eb4434518288bd42cbe
YONG WERN JIE <yongwernjie.2003@gmail.com>
2025-09-11 12:19:19 +0800

    npm run build

---
+++ public/build/assets/ApplicationLogo-IwStMTlw.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-ZtklitGL.js";function i(t){return e.jsx("div",{...t,className:"text-2xl font-bold text-indigo-600 flex justify-center items-center",children:"Nexscholar"})}export{i as A};
